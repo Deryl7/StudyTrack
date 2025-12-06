67a4142787d188971d855713ab540b21a5d751cf
Deryl7 <deryltammu7@gmail.com>
2025-12-07 03:46:26 +0800

    deleted dummy test from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-// HAPUS BARIS INI: import 'firebase_options.dart';
-import 'services/database_service.dart';
-import 'services/seed_data.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // KITA GANTI BAGIAN INI:
-  // Cukup panggil initializeApp tanpa parameter options.
-  // Karena kamu sudah pasang google-services.json, Flutter akan otomatis
-  // membaca konfigurasi dari file tersebut.
   await Firebase.initializeApp();
-
   runApp(const MyApp());
 }
 
@@ -22,35 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Backend Test Area")),
-        body: Center(
-          child: Builder(
-            builder: (context) {
-              return ElevatedButton(
-                onPressed: () async {
-                  // 1. Pura-pura Login (Pakai ID Sembarang dulu untuk tes)
-                  String fakeUserId = "test_user_mahasiswa_1";
-
-                  // 2. Inisialisasi Service
-                  DatabaseService db = DatabaseService(uid: fakeUserId);
-                  SeedData seeder = SeedData(dbService: db);
-
-                  // 3. Eksekusi
-                  await seeder.uploadDummyData();
-
-                  // 4. Feedback
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text("Data Dummy Terkirim ke Firebase!"),
-                    ),
-                  );
-                },
-                child: const Text("UPLOAD DATA DUMMY"),
-              );
-            },
-          ),
-        ),
+      title: 'StudyTrack',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(
+        body: Center(child: Text("Halaman Login akan dibuat oleh Tim UI")),
       ),
     );
   }
